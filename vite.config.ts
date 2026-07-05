cf98979a445cfa6ce894efaabc08a85e51941fea
kk <Kambledilip925@gmail.com>
2026-07-05 16:23:37 +0530

    Add GitHub Pages deploy and fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
+  base: '/rahul-shilpa-anniversary/',
   plugins: [react()],
-  base: './',
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
